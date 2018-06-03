32c446574499d226c6327df0529da4cc10bf6ca5
JNaeder <j.naeder324@gmail.com>
2018-06-02 20:04:50 -0400

    UPdated Mroe Things

--- WeeklyGameJam 47 - Thief/Assets/Scripts/Camera.cs
+++ WeeklyGameJam 47 - Thief/Assets/Scripts/Camera.cs
@@ -4,25 +4,22 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour {
 
-	public Transform camPoint;
+	public Transform camPoint, point1, point2, movingPoint;
 	public LineRenderer lineRen;
 	public float length;
 
 	public float speed;
-	public float movementDegree;
 	public float thresh;
 
 	bool moveForward;
 
 	Quaternion transRot;
-	Vector3 startAngle, newAngle;
+    Vector3 dir;
+    float dist, newAngle;
 
 	// Use this for initialization
 	void Start () {
-		startAngle = camPoint.rotation.eulerAngles;
-		newAngle = new Vector3(0, 0, movementDegree);
-		moveForward = true;
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -36,30 +33,32 @@ public class Camera : MonoBehaviour {
 
 
 	void MoveCam(){
-		transRot = camPoint.rotation;
-		float diff;
-		if(moveForward){
-			transRot.eulerAngles = Vector3.Lerp(transRot.eulerAngles, newAngle, speed * Time.deltaTime);
-			diff = newAngle.z - transRot.eulerAngles.z;
-			if(diff < thresh){
-				moveForward = !moveForward;
-			}
+        dir = movingPoint.position - camPoint.position;
+        newAngle = (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg) + 270;
 
-		} else {
-			transRot.eulerAngles = Vector3.Lerp(transRot.eulerAngles,startAngle, speed * Time.deltaTime);
-			diff = transRot.eulerAngles.z - startAngle.z;
-			if (diff < thresh)
-            {
-				moveForward = !moveForward;
 
+
+
+        if (moveForward) {
+
+            movingPoint.position = Vector3.MoveTowards(movingPoint.position, point2.position, speed * Time.deltaTime);
+            dist = Vector3.Distance(point2.position, movingPoint.position);
+            if (dist < thresh) {
+                moveForward = !moveForward;
             }
-		}
+            }
+        else {
+            movingPoint.position = Vector3.MoveTowards(movingPoint.position, point1.position, speed * Time.deltaTime);
+            dist = Vector3.Distance(point1.position, movingPoint.position);
+            if (dist < thresh)
+            {
+                moveForward = !moveForward;
+            }
+        }
 
-		camPoint.rotation = transRot;
+        camPoint.rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
 
-
-
-	}
+    }
 
 
 	void CheckForHit(){
@@ -70,10 +69,13 @@ public class Camera : MonoBehaviour {
 		lineRen.SetPosition(0, camPoint.position);
 		lineRen.SetPosition(1, hit.point);
 		Debug.DrawRay(camPoint.position, camPoint.up * dist, Color.red);
-        if (hit.collider.gameObject.tag == "Player")
+        if (hit.collider != null)
         {
-            print("SEE PLAYER!");
+            if (hit.collider.gameObject.tag == "Player")
+            {
+                print("SEE PLAYER!");
 
+            }
         }
 	}
 }
